97d5452d6d47a850147ffd7dbe0930ef53fb1e58
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-04-24 14:18:40 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Drum Machine</header>
+    </div>
+  );
+}
+
+export default App;
